d3d221709d32f6ba888bd629766af3f6c78addd4
Tet <tetiana_brzezinska@bonify.de>
2022-11-04 08:30:02 +0100

    DSC-80 added footer

--- src/index.jsx
+++ src/index.jsx
@@ -12,14 +12,14 @@ import {
   AppProvider,
   ErrorPage,
 } from '@edx/frontend-platform/react';
+import { getConfig } from '@edx/frontend-platform/config';
+import { getAuthenticatedUser } from '@edx/frontend-platform/auth';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Route, Switch } from 'react-router-dom';
 
-import Header, { messages as headerMessages } from '@edx/frontend-component-header';
-import { Footer } from '@woven-dojo/dojo-frontend-common/dist/components';
-
+import { Header, Footer } from '@woven-dojo/dojo-frontend-common/dist/components';
 import appMessages from './i18n';
 import { ProfilePage, NotFoundPage } from './profile';
 import configureStore from './data/configureStore';
@@ -27,9 +27,42 @@ import configureStore from './data/configureStore';
 import './index.scss';
 
 subscribe(APP_READY, () => {
+  const { BASE_URL, ORDER_HISTORY_URL, LOGOUT_URL } = getConfig();
+  const { username } = getAuthenticatedUser();
   ReactDOM.render(
     <AppProvider store={configureStore()}>
-      <Header />
+      <Header
+        logoDestination={`${BASE_URL}/dashboard`}
+        username={username}
+        userMenu={[
+          {
+            label: 'Dashboard',
+            href: `${BASE_URL}/dashboard`,
+          },
+          {
+            label: 'Profile',
+            href: `${BASE_URL}/u/${username}`,
+          },
+          {
+            label: 'Account',
+            href: `${BASE_URL}/account/settings`,
+          },
+          {
+            label: 'Order History',
+            href: ORDER_HISTORY_URL,
+          },
+          {
+            label: 'Logout',
+            href: LOGOUT_URL,
+          },
+        ]}
+        actionMenu={[
+          {
+            label: 'Courses',
+            href: `${BASE_URL}/dashboard`,
+          },
+        ]}
+      />
       <main>
         <Switch>
           <Route path="/u/:username" component={ProfilePage} />
@@ -48,10 +81,7 @@ subscribe(APP_INIT_ERROR, (error) => {
 });
 
 initialize({
-  messages: [
-    appMessages,
-    headerMessages,
-  ],
+  messages: [appMessages],
   requireAuthenticatedUser: true,
   hydrateAuthenticatedUser: true,
   handlers: {
